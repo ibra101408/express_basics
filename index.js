da345c3765cc3dd3090d4fa25d77ccc26c9f1b46
Daniil <daniil.vodenejev@khk.ee>
2022-02-03 11:50:47 +0200

    template loop use

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/questions', (req, res) =>{
+    let questions = [
+        {title: "What is Node.js", user : "Kadi", votes : "10"},
+        {title: "What is Express.js", user : "Mike", votes : "8"}
+    ]
+    res.render('index', {question:questions})
+})
+
+app.listen(3000, ()=> {
+    console.log('Server started on http://localhost:3000')
+})
+/*
+
 app.get('/user/:username', (req, res)=>{
     let user = req.params.username;
     res.render('index.ejs', {username : user});
 });
-
-app.listen(3000, ()=> {
-    console.log('Server started on http://localhost:3000');
-});
-/*
 app.get('/demo',(req, res)=>{
     res.render('index.ejs');
 });
