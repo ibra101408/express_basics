d44804e3c0ea6ced6d03d20454ee42720fb1170e
Daniil <daniil.vodenejev@khk.ee>
2022-02-03 11:35:47 +0200

    templating directory setup

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/user/:username', (req, res)=>{
     let user = req.params.username;
